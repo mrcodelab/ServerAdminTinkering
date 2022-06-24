fc18753c6767c8855a453c8f4a8fe62212736024
mrcodelab <96273662+mrcodelab@users.noreply.github.com>
2022-06-24 12:10:07 -0500

    Update psHashCompiler.ps1

--- Windows/psHashCompiler.ps1
+++ Windows/psHashCompiler.ps1
@@ -2,7 +2,6 @@ $rootpath = $HOME
 $pathway = $(Read-Host "Where is the target file")
 Set-Location $rootpath\$pathway
 $hpath = "$rootpath\GitHub\hashes"
-Write-Host $hpath
 $htarget = Read-Host "What file are you hashing?"
 $htext = Read-Host "What do you want your file called?"
 $htext = "$htext.txt"
